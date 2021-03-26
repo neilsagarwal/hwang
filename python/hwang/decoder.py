34ea9594a91b5f92977614612e03c67955e4073b
Neil <neilsagarwal@ucla.edu>
2021-03-25 22:38:52 -0700

    Convert retrieve function to a generator.

--- python/hwang/decoder.py
+++ python/hwang/decoder.py
@@ -67,3 +67,43 @@ class Decoder(object):
                                                len(valid_frames))
 
         return frames
+
+    def retrieve_generator(self, rows):
+        # Grab video index intervals
+        video_intervals = slice_into_video_intervals(self.video_index, rows)
+        frames = []
+        sample_offsets = self.video_index.sample_offsets()
+        sample_sizes = self.video_index.sample_sizes()
+        sample_offsets.append(sample_offsets[-1] + sample_sizes[-1])
+        sample_sizes.append(0)
+
+        for (start_index, end_index), valid_frames in video_intervals:
+            # Figure out start and end offsets
+            start_offset = sample_offsets[start_index]
+            end_offset = (sample_offsets[end_index] + sample_sizes[end_index])
+            # Read data buffer
+            self.f.seek(start_offset, 0)
+            encoded_data = self.f.read(end_offset - start_offset)
+
+            data = EncodedData()
+            data.width = self.video_index.frame_width()
+            data.height = self.video_index.frame_height()
+            data.format = self.video_index.format()
+            data.start_keyframe = start_index
+            data.end_keyframe = end_index
+
+            data.sample_offsets = ([
+                o - start_offset for o in sample_offsets[start_index:end_index]
+            ])
+            data.sample_sizes = (sample_sizes[start_index:end_index])
+            data.valid_frames = valid_frames
+            data.keyframes = [
+                k for k in self.video_index.keyframe_indices()
+                if k >= start_index and k <= end_index
+            ]
+            data.encoded_video = encoded_data
+            args = [data]
+            self._decoder.initialize(args, self.video_index.metadata_bytes())
+            for curr_frame in self._decoder.get_frames(self.video_index, len(valid_frames)):
+                yield curr_frame
+
